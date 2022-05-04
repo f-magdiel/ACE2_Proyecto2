22cb32fca0dc63f7767d022b753d32fd760c51c7
Ronald-CP <3004377060101@ingenieria.usac.edu.gt>
2022-05-03 21:33:32 -0600

    se anadio el gitignore

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -28,10 +28,10 @@ const Home = () => {
         axios.get('http://localhost:5500/data').then(res => {
             console.log(res.data)
             const object = res.data[0]
-            object.SuciedadAgua =object.SuciedadAgua.toFixed(2)
-            object.Humedad =object.Humedad.toFixed(2)
-            object.Filtrado =object.Filtrado.toFixed(2)
-            object.Agua =object.Agua.toFixed(2)
+            object.Metano =object.Metano.toFixed(2)
+            object.Temperatura =object.Temperatura.toFixed(2)
+            object.GeneradorChispa =object.GeneradorChispa.toFixed(2)
+            object.Gas =object.Gas.toFixed(2)
             setLatsData(object)
         })
     }
@@ -48,16 +48,16 @@ const Home = () => {
         if (last20.length > 0) {
             last20.map(x => {
                 if (index === 1) {
-                    data.unshift(x.SuciedadAgua)
+                    data.unshift(x.Metano)
 
                 } else if (index === 2) {
-                    data.unshift(x.Humedad)
+                    data.unshift(x.Temperatura)
 
                 } else if (index === 3) {
-                    data.unshift(x.Agua)
+                    data.unshift(x.Gas)
 
                 } else if (index === 4) {
-                    data.unshift(x.Filtrado)
+                    data.unshift(x.GeneradorChispa)
 
                 }
                 labels.unshift(x.Fecha)
@@ -76,19 +76,19 @@ const Home = () => {
                 <div class="container">
                     <div class="row">
                         <div class="col">
-                            <Card title={'Suciedad del Agua'} icon={'bi bi-emoji-dizzy-fill'} cantidad={lastData.SuciedadAgua + ' fotodiodos'} fecha={lastData.Fecha} />
+                            <Card title={'Cantidad Metano'} icon={'bi bi-emoji-dizzy-fill'} cantidad={lastData.Metano + ' fotodiodos'} fecha={lastData.Fecha} />
                             <div />
                         </div>
                         <div class="col">
-                            <Card title={'Humedad'} icon={'bi bi-cloud-haze-fill'} cantidad={lastData.Humedad + ' %'} fecha={lastData.Fecha} />
+                            <Card title={'Temperatura'} icon={'bi bi-cloud-haze-fill'} cantidad={lastData.Temperatura + ' %'} fecha={lastData.Fecha} />
                             <div />
                         </div>
                         <div class="col">
-                            <Card title={'Filtrado'} icon={'bi bi-funnel-fill'} cantidad={lastData.Filtrado + ' fotodiodos'} fecha={lastData.Fecha} />
+                            <Card title={'Gas'} icon={'bi bi-droplet-half'} cantidad={lastData.Gas + ' cm/s'} fecha={lastData.Fecha} />
                             <div />
                         </div>
                         <div class="col">
-                            <Card title={'Agua'} icon={'bi bi-droplet-half'} cantidad={lastData.Agua + ' cm/s'} fecha={lastData.Fecha} />
+                            <Card title={'Generador de Chispa'} icon={'bi bi-funnel-fill'} cantidad={lastData.GeneradorChispa + ' fotodiodos'} fecha={lastData.Fecha} />
                             <div />
                         </div>
 
@@ -100,9 +100,9 @@ const Home = () => {
                         <div className="my-3" style={{ maxWidth: '300px', marginLeft: 'auto', marginRight: 'auto' }}>
                             <Dropdown items={[{ name: "Suciedad VS tiempo (vivienda)", value: '1' },
                             { name: "Suelo VS tiempo", value: '2' },
-                            { name: "Agua VS tiempo", value: '3' },
+                            { name: "Gas VS tiempo", value: '3' },
                             { name: "Suciedad VS tiempo (filtrado)", value: 4 },
-                            { name: "Cantidad de agua", value: 5 }]}
+                            { name: "Cantidad de Gas", value: 5 }]}
                                 onChange={(e) => changeGraph(e)} />
                         </div>
 
